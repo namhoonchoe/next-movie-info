9b0ac7f03a7161b0e76311b0bdce3342dac41c29
Namhoon Choe <sysynh2012@gmail.com>
2024-01-21 20:52:22 +0900

    [Feat]  케러셀 슬라이더 #2
    컴포넌트 분리하고 애니메이션 추가중

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { movieApi } from "@/libs/api";
-import { imageDefaultUrl } from "@/utils/constants";
 import { Carousel } from "flowbite-react";
 import useSWR from "swr";
-import CarouselSlieder from "../ui/CarouselSlieder";
+import CarouselSlider from "../ui/CarouselSlider";
 import HeroHeaderItem from "../ui/HeroHeaderItem";
 
 async function tmdbFetcher(url: string) {
@@ -118,95 +117,26 @@ const HomePage: React.FC = () => {
       <main className="w-[72rem] flex flex-col items-start justify-center gap-16 my-12">
         <section className="flex flex-col justify-center items-start w-full">
           <p className="custom-heading">지금 뜨는 영화</p>
-          <div className="w-full h-96 border">
-            <>
-              {tmLoading && <div className="skeleton w-full h-full"></div>}
-              {tmData && (
-                <CarouselSlieder>
-                  {tmData.slice(0, 5).map((trendingMovie: any) => (
-                    <div
-                      className="w-44 h-60 rounded-md"
-                      key={trendingMovie.id}
-                    >
-                      <img
-                        src={`${imageDefaultUrl}/${trendingMovie.poster_path}`}
-                        alt={trendingMovie.title}
-                        className="w-full h-full overflow-hidden object-cover object-left-top"
-                      />
-                    </div>
-                  ))}
-                </CarouselSlieder>
-              )}
-            </>
+          <div className="w-full h-80">
+            <CarouselSlider dataList={tmData} isLoading={tmLoading} />
           </div>
         </section>
         <section className="flex flex-col justify-center items-start w-full">
           <p className="custom-heading">지금 뜨는 시리즈</p>
-          <div className="w-full h-96 border">
-            <>
-              {tsLoading && <div className="skeleton w-full h-full"></div>}
-              {tsData && (
-                <CarouselSlieder>
-                  {tsData.slice(0, 5).map((trendingSeries: any) => (
-                    <div
-                      className="w-44 h-60 rounded-md"
-                      key={trendingSeries.id}
-                    >
-                      <img
-                        src={`${imageDefaultUrl}/${trendingSeries.poster_path}`}
-                        alt={trendingSeries.title}
-                        className="w-full h-full overflow-hidden object-cover object-left-top"
-                      />
-                    </div>
-                  ))}
-                </CarouselSlieder>
-              )}
-            </>
+          <div className="w-full h-80  ">
+            <CarouselSlider dataList={tsData} isLoading={tsLoading} />
           </div>
         </section>
         <section className="flex flex-col justify-center items-start w-full">
           <p className="custom-heading">개봉예정 영화</p>
-          <div className="w-full h-96 border">
-            <>
-              {umLoading && <div className="skeleton w-full h-full"></div>}
-              {umData && (
-                <CarouselSlieder>
-                  {umData.slice(0, 5).map((upcommingMovie: any) => (
-                    <div
-                      className="w-44 h-60 rounded-md"
-                      key={upcommingMovie.id}
-                    >
-                      <img
-                        src={`${imageDefaultUrl}/${upcommingMovie.poster_path}`}
-                        alt={upcommingMovie.title}
-                        className="w-full h-full overflow-hidden object-cover object-left-top"
-                      />
-                    </div>
-                  ))}
-                </CarouselSlieder>
-              )}
-            </>
+          <div className="w-full h-80  ">
+            <CarouselSlider dataList={umData} isLoading={umLoading} />
           </div>
         </section>
         <section className="flex flex-col justify-center items-start w-full">
           <p className="custom-heading">방영 중인 TV 프로그램</p>
-          <div className="w-full h-96 border">
-            <>
-              {oaLoading && <div className="skeleton w-full h-full"></div>}
-              {oaData && (
-                <CarouselSlieder>
-                  {oaData.slice(0, 5).map((onAir: any) => (
-                    <div className="w-44 h-60 rounded-md" key={onAir.id}>
-                      <img
-                        src={`${imageDefaultUrl}/${onAir.poster_path}`}
-                        alt={onAir.title}
-                        className="w-full h-full overflow-hidden object-cover object-left-top"
-                      />
-                    </div>
-                  ))}
-                </CarouselSlieder>
-              )}
-            </>
+          <div className="w-full h-80  ">
+            <CarouselSlider dataList={oaData} isLoading={oaLoading} />
           </div>
         </section>
       </main>
