bd619e5190760a188e71b603db9140e155981caa
Namhoon Choe <sysynh2012@gmail.com>
2024-02-07 16:42:03 +0900

    [Feat] 회원가입
    Next Auth + React hook form 이용해서 회원가입 로직 구현

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,32 +1,54 @@
 import { auth } from "@/libs/firebase";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
 
 export default NextAuth({
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {},
       async authorize(credentials): Promise<any> {
-        return await signInWithEmailAndPassword(auth, (credentials as any).email || '', (credentials as any).password || '')
-          .then(userCredential => {
-            if (userCredential.user) {
-              return userCredential.user;
-            }
-            return null;
-          })
-          .catch(error => (console.log(error)))
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(error);
-  });
-      }
-    })
+        try {
+          await signInWithEmailAndPassword(
+            auth,
+            (credentials as any).email || "",
+            (credentials as any).password || ""
+          );
+        } catch(e) {
+          if(e instanceof FirebaseError) {
+            throw new Error(e.message);
+          }
+        } finally {
+          console.log("authorized!")
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!
+    }),
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!
+    }),
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
+  }) as Adapter,
   pages: {
     signIn: "/signin",
-    newUser:"/signup"
+    newUser: "/signup",
   },
 });
--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -1,44 +1,87 @@
-import { auth } from '@/libs/firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
+import { auth } from "@/libs/firebase";
+import { FirebaseError } from "firebase/app";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+  email: string;
+  username: string;
+  password: string;
+  checkPassword: string;
+}
 
 export default function SignupPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordAgain, setPasswordAgain] = useState('');
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IFormInput>();
 
-  const signup = () => {
-    createUserWithEmailAndPassword(auth, email, password);
+  const signUp = async (email: string, password: string) => {
+    try {
+      const credentials = createUserWithEmailAndPassword(auth, email, password);
+
+      await updateProfile((await credentials).user, {
+        displayName: watch("username"),
+      });
+      router.push("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+      }
+    } finally {
+    }
   };
-  
+  const onSubmit = () => signUp(watch("email"), watch("password"));
+
   return (
-
     <div className="w-full max-w-[338px] mt-20 flex flex-col justify-center px-6 py-12 lg:px-8 border rounded-xl shadow *:text-slate-600">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">
-            Sign up
-          </h2>
-        </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">
+          Sign up
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 ">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 "
+              >
                 Email address
               </label>
               <div className="mt-2">
                 <input
                   id="email"
-                  name="email"
                   type="email"
                   autoComplete="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
+                  {...register("email", { required: true })}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 "
+              >
+                User Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  type="name"
+                  autoComplete="name"
+                  {...register("username", { required: true })}
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -46,36 +89,38 @@ export default function SignupPage() {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 ">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 "
+                >
                   Password
                 </label>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
-                  name="password"
                   type="password"
                   autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
+                  {...register("password", { required: true })}
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 ">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 "
+                >
                   Password Again
                 </label>
               </div>
               <div className="mt-2">
                 <input
-                  id="passwordAgain"
-                  name="passwordAgain"
+                  id="password"
                   type="password"
                   autoComplete="current-password"
-                  onChange={(e) => setPasswordAgain(e.target.value)}
-                  required
+                  {...register("checkPassword", { required: true })}
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -83,24 +128,25 @@ export default function SignupPage() {
 
             <div>
               <button
-                disabled={(!email || !password || !passwordAgain) || (password !== passwordAgain)}
-                onClick={() => signup()}
+                disabled={
+                  !watch("email") ||
+                  !watch("password") ||
+                  !watch("checkPassword") ||
+                  watch("password") !== watch("checkPassword")
+                }
+                type="submit"
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign Up
               </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
-     
-  )
+    </div>
+  );
 }
 
-
- 
-SignupPage.getLayout = function getLayout(page:React.ReactElement) {
-  return (
-      page
-  )
-}
+SignupPage.getLayout = function getLayout(page: React.ReactElement) {
+  return <div className="fit-center">{page}</div>;
+};
